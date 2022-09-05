5383a75e12eede262eebac03e8c760396978825c
H4rvest3r <110695494+H4rvest3r@users.noreply.github.com>
2022-09-05 11:29:33 -0300

    Delete FUNCTIONS_LAYOUT directory

--- FUNCTIONS_LAYOUT/modelos/footer.php
+++
@@ -1,5 +0,0 @@
-    <div class="footer">
-                <span class="copy">Copyright ETEC Palmital &copy; 2017</span>
-            </div>
-         </body>
-     </html>
--- FUNCTIONS_LAYOUT/modelos/navbar.php
+++
@@ -1,8 +0,0 @@
-<div class="navbar">
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="sobre.php">Sobre o PHP</a></li>
-                        <li><a href="#">Fissura Profunda</a></li>
-                    </ul>
-                </div>
-            </div>
