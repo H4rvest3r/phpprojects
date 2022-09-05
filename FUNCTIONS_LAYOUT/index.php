9ee2e4c831c2a93b334bf2b3190b0ba6d2ed32e9
H4rvest3r <110695494+H4rvest3r@users.noreply.github.com>
2022-09-05 11:27:08 -0300

    Add files via upload

---
+++ FUNCTIONS_LAYOUT/index.php
@@ -0,0 +1,11 @@
+<?php require_once 'functions.php' ?>
+
+<?php get_header(); ?>
+
+            <div class="container">
+                <h2>Vendemos Pão</h2>
+                <p>Aplicativo de exemplo utilizando PHP, HTML e CSS</p>
+                <img src="http://bit.ly/2qW1i0G" alt="Html5 & CSS3 Logos"/>
+            </div>
+
+<?php get_footer(); ?>        
---
+++ FUNCTIONS_LAYOUT/modelos/header.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Teste</title>
+        <link href="recursos/css/style.css" rel="stylesheet"/>
+        </head>
+        <body>
+            <div class="header">
+                <h1>Padaria do Décio</h1>
+                
+                <?php get_model('navbar'); ?>
+            
+            </div>
